efc258328a2e5ea69af65096ef58f4cb92f4012a
Jayden Kim <hkim@lcs.on.ca>
2024-10-28 09:18:03 -0400

    Initial Commit

---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by kim heejun on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
