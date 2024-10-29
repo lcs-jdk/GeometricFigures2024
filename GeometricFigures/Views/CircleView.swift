501199d03e62b071982c6dedb3698420db404ea8
Jayden Kim <hkim@lcs.on.ca>
2024-10-29 11:32:23 -0400

    added circle structure to our model and made a basic view to control the radius.

---
+++ GeometricFigures/Views/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  GeometricFigures
+//
+//  Created by kim heejun on 10/28/24.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
---
+++ GeometricFigures/model/Circle.swift
@@ -0,0 +1,8 @@
+//
+//  Circle.swift
+//  GeometricFigures
+//
+//  Created by kim heejun on 10/28/24.
+//
+
+import Foundation
