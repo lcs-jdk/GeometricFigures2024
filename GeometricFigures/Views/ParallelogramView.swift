881975dfe92e4bea29345927e45e11012d3d79d0
Jayden Kim <hkim@lcs.on.ca>
2024-10-31 15:13:27 -0400

    final version of my work

---
+++ GeometricFigures/Views/ParallelogramView.swift
@@ -0,0 +1,18 @@
+//
+//  ParallelogramView.swift
+//  GeometricFigures
+//
+//  Created by kim heejun on 10/29/24.
+//
+
+import SwiftUI
+
+struct ParallelogramView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParallelogramView()
+}
---
+++ GeometricFigures/model/Parallelogram.swift
@@ -0,0 +1,8 @@
+//
+//  Parallelogram.swift
+//  GeometricFigures
+//
+//  Created by kim heejun on 10/29/24.
+//
+
+import Foundation
